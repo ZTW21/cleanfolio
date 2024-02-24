f4d35f58e048b86547ada8b244cb1f214b606313
ZTW21 <ztwilson2002@gmail.com>
2024-02-24 13:02:56 -0500

    added resume

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://github.com/ZTW21/cleanfolio'
       className='link footer__link'
     >
       Updated Spring 2024
